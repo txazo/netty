d13339ac939610dd262713bc00e1a539d0eeb944
txazo <784990655@qq.com>
2017-02-17 00:02:40 +0800

    update

--- src/test/java/test/netty/echo/EchoServerHandler.java
+++ src/test/java/test/netty/echo/EchoServerHandler.java
@@ -8,6 +8,7 @@ public class EchoServerHandler extends ChannelInboundHandlerAdapter {
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
         ctx.write(msg);
+        ctx.channel().write("hello");
         ctx.flush();
     }
 
